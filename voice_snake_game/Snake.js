da8ef4d2c6403d685a745bec18ce25ce6e179f52
Jiahe Yan <yjh020711@ucla.edu>
2020-12-30 14:31:33 +0800

    created a keyboard-based snake game

--- voice_snake_game/Snake.js
+++ voice_snake_game/Snake.js
@@ -1,14 +1,10 @@
 class Snake {
     constructor() {
         this.body = [];
-        this.body[0] = createVector(0, 0);
+        this.body[0] = createVector(floor(w / 2), floor(h / 2));
         this.xdir = 0;
         this.ydir = 0;
-    }
-
-    update() {
-        this.body[0].x += this.xdir;
-        this.body[0].y += this.ydir;
+        this.len = 0;
     }
 
     setDir(x, y) {
@@ -16,8 +12,63 @@ class Snake {
         this.ydir = y;
     }
 
+    update() {
+        let head = this.body[this.body.length - 1].copy();
+        this.body.shift();
+        head.x += this.xdir;
+        head.y += this.ydir;
+        this.body.push(head);
+    }
+
+    grow() {
+        let head = this.body[this.body.length - 1].copy();
+        this.len++;
+        this.body.push(head);
+    }
+
+    endGame() {
+        let x = this.body[this.body.length - 1].x;
+        let y = this.body[this.body.length - 1].y;
+        if (x > w - 1 || x < 0 || y > h - 1 || y < 0) {
+            return true;
+        }
+        for (let i = 0; i < this.body.length - 1; i++) {
+            let part = this.body[i];
+            if (part.x == x && part.y == y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    eat(pos) {
+        let x = this.body[this.body.length - 1].x;
+        let y = this.body[this.body.length - 1].y;
+        if (x == pos.x && y == pos.y) {
+            this.grow();
+            return true;
+        }
+        return false;
+    }
+
     show() {
-        fill(0);
-        rect(this.body[0].x, this.body[0].y, 10, 10);
+        for (let i = 0; i < this.body.length; i++) {
+            fill(0);
+            noStroke();
+            rect(this.body[i].x, this.body[i].y, 1, 1);
+        }
+    }
+
+    getLen() {
+        return this.body.length;
+    }
+
+    hasLoc(x, y) {
+        for (let i = 0; i < this.body.length; i++) {
+            if (x === this.body[i].x && y === this.body[i].y) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- voice_snake_game/voice_recognition.js
+++ voice_snake_game/voice_recognition.js
@@ -11,6 +11,12 @@ let soundModelURL = 'https://teachablemachine.withgoogle.com/models/V1geI3hYG/' 
 let label = "";
 
 let snake;
+let rez = 10;
+let food;
+let score;
+
+let w;
+let h;
 
 // Load the model first
 function preload() {
@@ -20,10 +26,28 @@ function preload() {
 function setup() {
     let cnv = createCanvas(400, 400);
     cnv.parent("canvasContainer");
+    frameRate(5);
+
+    w = floor(width / rez);
+    h = floor(height / rez);
 
     snake = new Snake();
+    score = snake.getLen();
+
+    foodLocation();
 
     classifyAudio();
+}
+
+function foodLocation() {
+    let x = floor(random(w));
+    let y = floor(random(h));
+
+    while (snake.hasLoc(x, y)) {
+        x = floor(random(w));
+        y = floor(random(h));
+    }
+    food = createVector(x, y);
 }
 
 function keyPressed() {
@@ -39,10 +63,28 @@ function keyPressed() {
 }
 
 function draw() {
+    scale(rez);
+
     background(220);
 
+    if (snake.eat(food)) {
+        foodLocation();
+        score = snake.getLen();
+        let score_string = document.getElementById('snake-score');
+        score_string.innerText = "Score: " + score;
+    }
     snake.update();
     snake.show();
+
+    if (snake.endGame()) {
+        print('END GAME');
+        background(255, 0, 0);
+        noLoop();
+    }
+
+    noStroke();
+    fill(255, 0, 0);
+    rect(food.x, food.y, 1, 1);
 }
 
 function classifyAudio() {
