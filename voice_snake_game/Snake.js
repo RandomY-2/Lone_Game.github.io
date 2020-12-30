8ae5808ac34fde8c3b3e016ed4d98f1d60d34cc4
Jiahe Yan <yjh020711@ucla.edu>
2020-12-30 10:28:03 +0800

    started to implement the keyboard-controlled snake game

---
+++ voice_snake_game/Snake.js
@@ -0,0 +1,23 @@
+class Snake {
+    constructor() {
+        this.body = [];
+        this.body[0] = createVector(0, 0);
+        this.xdir = 0;
+        this.ydir = 0;
+    }
+
+    update() {
+        this.body[0].x += this.xdir;
+        this.body[0].y += this.ydir;
+    }
+
+    setDir(x, y) {
+        this.xdir = x;
+        this.ydir = y;
+    }
+
+    show() {
+        fill(0);
+        rect(this.body[0].x, this.body[0].y, 10, 10);
+    }
+}
--- voice_snake_game/voice_recognition.js
+++ voice_snake_game/voice_recognition.js
@@ -10,13 +10,39 @@ let soundModelURL = 'https://teachablemachine.withgoogle.com/models/V1geI3hYG/' 
 // To store the classification
 let label = "";
 
+let snake;
+
 // Load the model first
 function preload() {
     classifier = ml5.soundClassifier(soundModelURL);
 }
 
 function setup() {
+    let cnv = createCanvas(400, 400);
+    cnv.parent("canvasContainer");
+
+    snake = new Snake();
+
     classifyAudio();
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+        snake.setDir(-1, 0);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.setDir(1, 0);
+    } else if (keyCode === UP_ARROW) {
+        snake.setDir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.setDir(0, 1);
+    }
+}
+
+function draw() {
+    background(220);
+
+    snake.update();
+    snake.show();
 }
 
 function classifyAudio() {
